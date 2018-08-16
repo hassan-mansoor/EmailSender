9a11319659411cc8946f9a93587a95f416eb3f5c
mansha <mansha@f-secure.com>
2018-08-16 12:46:10 +0200

    Initial Commit

---
+++ EmailSender/WpfApplication1/Mail/Mailer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace EmailSender.Mail
+{
+    class Mailer
+    {
+        public void CreateMaile(List<string> attachment_lst)
+        {
+            try {
+                  MailMessage message = new MailMessage();
+                  message.To.Add(new MailAddress("hassan_mansoor32@yahoo.com"));
+                  message.From = new MailAddress("put.hassanmansoor@gmail.com");
+                  message.Subject = "Sample subject";
+                  message.Body = CreateMailBody("Example body this is sample message");
+                  message.IsBodyHtml = true;
+             //     AddAttachments(message, attachment_lst);
+                  Console.WriteLine(message);
+                  SmtpClient smtp = new SmtpClient();
+                  smtp.Host = ConfigurationManager.AppSettings["Host"];
+                  smtp.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSsl"]);
+                  //System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                  //NetworkCred.UserName = ConfigurationManager.AppSettings["UserName"];
+                  //NetworkCred.Password = ConfigurationManager.AppSettings["Password"];
+                  smtp.UseDefaultCredentials = true;
+                  smtp.Timeout = 10000;
+                  smtp.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["UserName"], ConfigurationManager.AppSettings["Password"]);
+                  smtp.Port = int.Parse(ConfigurationManager.AppSettings["Port"]); //reading from web.config  
+                  smtp.Send(message);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in sendEmail:" + ex.Message);
+            }
+        }
+
+        private string CreateMailBody( string message)
+        {
+            string body = string.Empty;
+            StreamReader reader = new StreamReader("../../Templates/Template1.html");
+            body = reader.ReadToEnd();
+            body = body.Replace("{message}", message);
+            return body;
+        }
+
+        private void AddAttachments(MailMessage message, List<string> attachment_lst)
+        {
+            attachment_lst.ForEach(attachment => {
+                Attachment a = new Attachment(attachment);
+                message.Attachments.Add(a);
+            });
+
+        }
+    }
+}
---
+++ EmailSender/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmailSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> attachment_lst = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Title = "Clicked";
+            Mail.Mailer mailer = new Mail.Mailer();
+            mailer.CreateMaile(attachment_lst);
+          
+        }
+
+        private void Button_Upload(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            Nullable<bool> result = fileDialog.ShowDialog();
+            if (result == true)
+            {
+                string filename = fileDialog.FileName;
+                attachment_lst.Add(filename);
+                this.Title = filename;
+
+            }
+
+        }
+    }
+}
