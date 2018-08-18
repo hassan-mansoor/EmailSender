8f37ccc40672b83f0bbd70b13ac3c3205507502f
Hassan Mansoor <put.hassanmansoor@gmail.com>
2018-08-18 21:23:08 +0200

    Add files via upload
    Auto email sender

---
+++ SPECFILE_M/SPECFILE_M/CopyDirectories.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPECFILE_M
+{
+    class CopyDirectories
+    {
+       
+        public CopyDirectories()
+        {
+            string sourceFolder = @"..\..\..\..\WEJSCIE\";
+            string destFolder = @"..\..\..\..\BUFOR\";
+            if (IsDirectoryExist(sourceFolder))
+            {
+                CopyFolder(sourceFolder, destFolder);
+            }
+        }
+
+        private void CreateNewDirectory(string directory)
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        private bool IsDirectoryExist(string directory)
+        {
+            return Directory.Exists(directory);
+        }
+
+        private void CopyFiles(string sourceFolder, string destFolder)
+        {
+            string[] files = Directory.GetFiles(sourceFolder);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(destFolder, name);
+                File.Copy(file, dest);
+                RemoveFiles(file);
+            }
+        }
+
+        private void CopyFolder(string sourceFolder, string destFolder)
+        {
+            if (!IsDirectoryExist(destFolder))
+            {
+                CreateNewDirectory(destFolder);
+            }
+            CopyFiles(sourceFolder, destFolder);
+            string[] folders = Directory.GetDirectories(sourceFolder);
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(destFolder, name);
+                CopyFolder(folder, dest);
+                RemoveDirectory(folder);
+            }
+        }
+
+        private void RemoveFiles(string file)
+        {
+            File.Delete(file);
+        }
+
+        private void RemoveDirectory(string direcoty)
+        {
+            Directory.Delete(direcoty);
+        }
+
+    }
+}
---
+++ SPECFILE_M/SPECFILE_M/FilesReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPECFILE_M
+{
+    class FilesReader
+    {
+        public void ReadDirectories()
+        {
+            string sourceFolder = @"C:\Users\mansha\Documents\TestCopy\";
+
+            string[] folders = Directory.GetDirectories(sourceFolder);
+            foreach (string folder in folders)
+            {
+                string recipients = $"{folder}\\MailAddressesTo.txt";
+                string attachments = $"{folder}\\Attachments.txt";
+                string body = File.ReadAllText($"{folder}\\body.txt");
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("put.hassanmansoor@gmail.com");
+                ReadFileToAddRecipients(recipients, message);
+                message.Subject = "Korespondencja niejawna";
+                message.Body = body;
+                message.IsBodyHtml = true;
+                message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;
+                message.Priority = MailPriority.High;
+                ReadFileToAddAttachments(attachments,message);
+                try
+                {
+                    send(message);
+                }
+                catch (SmtpFailedRecipientException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+            }
+        }
+
+        public void ReadFileToAddRecipients(string filePath, MailMessage message)
+        {
+           foreach( string recipient in File.ReadLines(filePath))
+            {
+                message.To.Add(new MailAddress(recipient));
+            }
+           
+        }
+
+        public void ReadFileToAddAttachments(string filePath, MailMessage message)
+        {
+            foreach (string attachment in File.ReadLines(filePath))
+            {
+                Attachment a = new Attachment(attachment);
+                message.Attachments.Add(a);                    
+                
+            }
+        }
+
+        public void send(MailMessage message)
+        {
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential("put.hassanmansoor@gmail.com", "Hobnob101");
+            smtp.Port = 587; //reading from web.config  
+            smtp.Send(message);
+        }
+    }
+}
